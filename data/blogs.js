6a7d557b3e89062382e5d844af87e4afece875d2
systemattendance <systemattendance64@gmail.com>
2021-09-17 20:34:38 +0530

    Initial Commit

---
+++ data/blogs.js
@@ -0,0 +1,25 @@
+blogs = [
+    {
+        title: "How to get started with Python.",
+        content: "This is content Python",
+        slug: "learn-python"
+    },
+    {
+        title: "How to get started with JS.",
+        content: "This is content JS",
+        slug: "learn-js"
+    },
+    {
+        title: "How to get started with Django.",
+        content: "This is content Django",
+        slug: "learn-django"
+    },
+    {
+        title: "How to get started with CSS.",
+        content: "This is content CSS",
+        slug: "learn-css"
+    },
+]
+
+
+module.exports = blogs;
